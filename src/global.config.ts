39983d38394a88e3eb8de9976bfb867f003bfbb3
BlueGuy <blue@nalikes.com>
2025-01-10 19:39:28 +0300

    updated infura

--- src/global.config.ts
+++ src/global.config.ts
@@ -10,13 +10,13 @@ export const CHAIN_CONFIGS_DEV: ChainConfig[] = [
     {
         //bsc testnet
         "chainId": 97,
-        "rpcUrl": "https://bsc-testnet.infura.io/v3/2c3a1602edd64164b85fab409c12046b",
+        "rpcUrl": "https://bsc-testnet.infura.io/v3/c7849d996e1f4e7ea5b000d02be5e766",
         "contractAddress": "0xbA0805271367f8F88917a15eb1FE216CD15a9743"
     },
     {
         // eth sepolia
         "chainId": 11155111,
-        "rpcUrl": "https://sepolia.infura.io/v3/2c3a1602edd64164b85fab409c12046b",
+        "rpcUrl": "https://sepolia.infura.io/v3/c7849d996e1f4e7ea5b000d02be5e766",
         "contractAddress": "0xe4D9c42477B67Abf2372188c4a2B9948ca054911"
     },
     // {
